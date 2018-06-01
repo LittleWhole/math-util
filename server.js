00342b1b818c18ea6933579cf67c1af999773b60
Glitch (math-utils) <none>
2018-06-01 18:22:30 +0000

    🙉😎 Checkpoint ./server.js:491319/1180

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-exports.sum = fu
+exports.summation = function(values, operation, start) {
+  let result;
+  if (!start) result = 0;
+  else result = start;
+  if (operation == "addition") {
+    values.forEach(value => {
+      result += value;
+    });
+  }
+  else if (operation == "subtraction") {
+    values.forEach(value => {
+      result -= value;
+    });
+  }
+  else if (operation == "multiplication") {
+    values.forEach(value => {
+      result *= value;
+    });
+  }
+  else if (operation == "di
+  
+  return result;
+}
